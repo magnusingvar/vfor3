dfcea95787b16217833157500b49ec1eb338d451
Magnus Ingvar <magnusingvar@gmail.com>
2021-12-02 16:24:09 +0000

    lokaverkefni

--- lokaverkefni/routes/contact.js
+++ lokaverkefni/routes/contact.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   let transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
-    port: 25,
+    port: 2525,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
